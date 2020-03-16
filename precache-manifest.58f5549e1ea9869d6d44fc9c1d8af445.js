f304ee79385eb5b2efe21b5aa59cf6cd683f6c7f
SailorDev <sailorjs2020@gmail.com>
2020-03-17 00:23:22 +0200

    Updates

---
+++ precache-manifest.58f5549e1ea9869d6d44fc9c1d8af445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71137f407167f7e643129ddcbfbdc5e3",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "4759ba308083a9e0e3c7",
+    "url": "/monsters-react/static/css/main.82b8d9e3.chunk.css"
+  },
+  {
+    "revision": "e0f591c52035b19f6fec",
+    "url": "/monsters-react/static/js/2.52ec6830.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-react/static/js/2.52ec6830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4759ba308083a9e0e3c7",
+    "url": "/monsters-react/static/js/main.82258a22.chunk.js"
+  },
+  {
+    "revision": "b22b0b9ea0692db7c1c3",
+    "url": "/monsters-react/static/js/runtime-main.c33fe019.js"
+  }
+]);
